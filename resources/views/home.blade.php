d2015f09cccf140ffad2adc66f3da928d8062f8f
neeharika cheepurapalli <scheepurapalli@unomaha.edu>
2017-02-04 14:42:02 -0600

    adding neehaeagle to git repo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,7 @@
                 <div class="panel-heading">EAGLE FINANCIAL SERVICES</div>
 
                 <div class="panel-body">
-                    Eagle Financial Services provides financial services and provides advising services for the customer on investing and
-                    growing the financial portfolio! Established in 1990 and rapidly grown to be one of the leading financial company.
-                    Tracking all the stocks and investments is an important key for enhancing our future services.
-                    <br>
-                    <br>
-                    <a href="{{ url('/customers') }}">Hello!!!! Financial Advisor
-                        Click Here!!</a>
+                    <a href="{{ url('/customers') }}">Click Here to proceed!!!</a>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,9 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome</div>
                 <div class="panel-body" align="center">
-                    <img height="100px", width="100px" src="https://pgcpsmess.files.wordpress.com/2014/03/bald-eagle.jpg">
                     <h1><b>Eagle Financial Services</b></h1>
                 </div>
                 <div class="panel-body" align="right">
-                    <h3>We care your future...!</h3>
                 </div>
             </div>
         </div>
